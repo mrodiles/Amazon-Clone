80c0b30424b9eb1d9f51faaffb80531cc8ed71e5
“Michael <michaelrodiles@gmail.com>
2021-12-09 15:25:12 -0500

    product component and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 function Home() {
   return (
     <div className="home">
       <div className="home__container">
         <img
           className="home__image"
-          src="https://images-eu.ssl-images-amazon.com/images/G/02/UK-hq/2020/img/DVD/XCM_Manual_1200x445_1217661_1130674_IDx231217661_uk_dvd_digital_readiness_primevideo_banner_2_44c2b7ea_2f3b_4de2_ab69_9968a9e8112a_jpg_LOWER_QL85_.jpg"
+          src="https://m.media-amazon.com/images/G/01/digital/video/sonata/Superhero_UK_Acquisition_FT_Apr_20/7a2ef2c8-d54c-4da8-beba-f4e12a0f10d5._UR1280,600_SX1500_FMjpg_.jpg"
           alt=""
         />
+
+        <div className="home__row">
+          <Product />
+          {/* product */}
+        </div>
+        <div className="home__row">
+          {/* product */}
+          {/* product */}
+          {/* product */}
+        </div>
+        <div className="home__row">{/* products */}</div>
       </div>
     </div>
   );
---
+++ src/components/Product.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Product.css";
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The Lean Startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>🌟</p>
+        </div>
+      </div>
+      <img
+        src="https://is5-ssl.mzstatic.com/image/thumb/Publication2/v4/09/25/78/092578c0-202d-85a4-15f8-671efbb82f8e/9780307887917.jpg/1200x630wz.png"
+        alt=""
+      />
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
