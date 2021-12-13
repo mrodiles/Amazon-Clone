5844ed8c8ec9d61e232660b508af3193a163a000
“Michael <michaelrodiles@gmail.com>
2021-12-13 08:48:48 -0500

    Header Adjustment Image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ function Header() {
       <img
         alt=""
         className="header__logo"
-        src="https://clipartcraft.com/images/amazon-smile-logo-transparent-background-7.png"
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1250%2F3061%2Ffiles%2Fwhite-amazon-logo-png-6_1024x1024.png%3Fv%3D1539106025&f=1&nofb=1"
       />
 
       <div className="header__search">
