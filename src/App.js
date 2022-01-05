f767bb18211a9b8936ef15864aa581b453972e23
“Michael <michaelrodiles@gmail.com>
2022-01-05 14:13:50 -0500

    debugging react router issues

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from "react";
-import Header from "./components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
 import Home from "./components/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    //BEM convention for styling
-    <Router>
+    <BrowserRouter>
       <div className="app">
         <Routes>
-          <Route path="/checkout">
-            <Header />
-            <h1>I AM A CHECKOUT PAGE EXAMPLE!</h1>
-          </Route>
-          <Route path="/">
+          <Route exact path="/">
             <Header />
             <Home />
           </Route>
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
