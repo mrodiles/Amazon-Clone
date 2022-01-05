c3f0286738470bc7a91eb2920c99b27bcaeda747
“Michael <michaelrodiles@gmail.com>
2022-01-04 20:54:54 -0500

    checkout routes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import React from "react";
 import Header from "./components/Header/Header";
 import "./App.css";
 import Home from "./components/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM convention for styling
     <Router>
       <div className="app">
-      <Switch>
-      <Route path="/">
-        <Header />
-        <Home />
-
-      </Route>
-
-      </Switch>
+        <Routes>
+          <Route path="/checkout">
+            <Header />
+            <h1>I AM A CHECKOUT PAGE EXAMPLE!</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Routes>
       </div>
     </Router>
   );
